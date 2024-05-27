e51116a8ea78f4c36ec98645a885a783921745eb
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2024-05-27 15:53:16 +0530

    Add files via upload
    This data and clock is created by using the JavaScript

---
+++ clock.js
@@ -0,0 +1,36 @@
+function updateClock() {
+    const clock = document.getElementById('clock');
+    const now = new Date();
+    
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
+    clock.classList.remove('fade');
+    void clock.offsetWidth;
+    clock.classList.add('fade')
+}
+updateClock();
+setInterval(updateClock, 1000);
+
+function date(){
+    const newDate = document.getElementById('date');
+    const nowDate = new Date();
+    newDate.textContent = `${nowDate.getDate()}-${nowDate.getMonth()}-${nowDate.getUTCFullYear()}`;
+}
+date();
+
+function day(){
+    const currentDay = new Date();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const displayDay = days[currentDay.getDay()];
+    return displayDay;
+}
+
+function displayDayName(){
+    const dayName = day();
+    document.getElementById('day').innerText = dayName;
+}
+setInterval(displayDayName, 1000);
+window.onload = displayDayName;
